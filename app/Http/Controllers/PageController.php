ed79e7958f15d32be05aa0aea5be0e6e8fc804ab
gbelda <gbelda56@gmail.com>
2022-01-14 16:17:31 +0100

    Create database data w/ tinker and print on page

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Flight;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index(){
-            return view('home');
+            $flights = Flight::all();
+            return view('home', compact('flights'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,9 +7,27 @@
     </div>
 
     <section>
-        <div class="card">
-
-        </div>
+        @foreach ($flights as $flight)
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h4 class="card-title">
+                        {{ $flight->destination }}
+                    </h4>
+                    <h5>
+                        Airlines: {{ $flight->airline }}
+                    </h5>
+                    <h6>
+                        Flight Duration: {{ $flight->flight_duration }}
+                    </h6>
+                    <div class="d-flex align-items-center justify-content-evenly">
+                        <h5 class="m-0">
+                            {{ $flight->price }}&euro;
+                        </h5>
+                        <a href="#" class="btn btn-primary">Book Now</a>
+                    </div>
+                </div>
+            </div>
+        @endforeach
     </section>
 
 
