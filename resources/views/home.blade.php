6a3baa3a5718c4d701176e225c2a9f13d66c4d6b
gbelda <gbelda56@gmail.com>
2022-01-14 16:42:16 +0100

    add bootstrap styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,34 +1,39 @@
 @extends('layouts.app')
 @section('content')
-    <div class="title">
-        <h1>
-            Flights
-        </h1>
-    </div>
 
-    <section>
-        @foreach ($flights as $flight)
-            <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                    <h4 class="card-title">
-                        {{ $flight->destination }}
-                    </h4>
-                    <h5>
-                        Airlines: {{ $flight->airline }}
-                    </h5>
-                    <h6>
-                        Flight Duration: {{ $flight->flight_duration }}
-                    </h6>
-                    <div class="d-flex align-items-center justify-content-evenly">
-                        <h5 class="m-0">
-                            {{ $flight->price }}&euro;
-                        </h5>
-                        <a href="#" class="btn btn-primary">Book Now</a>
+    <main>
+        <div class="title">
+            <h1>
+                Flights
+            </h1>
+        </div>
+
+        <section>
+            <div class="row pt-3">
+                @foreach ($flights as $flight)
+                    <div class="card m-3" style="width: 18rem;">
+                        <div class="card-body">
+                            <h4 class="card-title">
+                                {{ $flight->destination }}
+                            </h4>
+                            <h5>
+                                Airlines: {{ $flight->airline }}
+                            </h5>
+                            <h6>
+                                Flight Duration: {{ $flight->flight_duration }}
+                            </h6>
+                            <div class="d-flex align-items-center justify-content-evenly">
+                                <h5 class="m-0">
+                                    Price: {{ $flight->price }}&euro;
+                                </h5>
+                                <a href="#" class="btn btn-primary">Book Now</a>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                @endforeach
             </div>
-        @endforeach
-    </section>
+        </section>
+    </main>
 
 
 @endsection
