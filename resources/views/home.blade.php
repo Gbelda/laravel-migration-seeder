2ba02fa870a7ae896c074d08230eb4335cf55300
gbelda <gbelda56@gmail.com>
2022-01-14 20:13:04 +0100

    finish welcome page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,8 +7,12 @@
 
 @section('content')
     <main>
-        <h1>
-            Welcome to Not a scam agency
-        </h1>
+        <div class="overlay d-flex flex-wrap justify-content-center align-items-center">
+            <h1>
+                Welcome to Not a scam agency
+            </h1>
+            <a class="btn btn-primary" href="{{ route('flights') }}" role="button">Check our offers!</a>
+        </div>
+
     </main>
 @endsection
